8cbafbbfe51b2097909457f9c9e0e2653c37dba2
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-08 09:15:33 -0400

    Make app refresh reload current content

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v6";
+const CACHE_NAME = "fbp-shell-v7";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
