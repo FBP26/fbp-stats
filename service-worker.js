9a0f80a782ebd143b4474af9646c7c9fd2b82a8a
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-09 18:26:40 -0400

    Refresh installed map asset

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v12";
+const CACHE_NAME = "fbp-shell-v13";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
