5f4c17a97d2d81bb2b6eda049b2efd7cdb11e171
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-09 18:17:07 -0400

    Keep Maps behind FAQ refresh

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v10";
+const CACHE_NAME = "fbp-shell-v11";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
