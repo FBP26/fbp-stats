51f8ce3ddf00caa6c8a5296a843c708a51ad4456
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-09 18:15:55 -0400

    Route app refresh from FAQ to Maps

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v9";
+const CACHE_NAME = "fbp-shell-v10";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
