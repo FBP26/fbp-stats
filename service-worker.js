5a981455524b04038ad90f4ec61de86f279a2950
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 10:21:49 -0400

    Keep active season awards provisional

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v15";
+const CACHE_NAME = "fbp-shell-v16";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
