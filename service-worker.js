0656ea77a571bd879d6d20cd5d728258eeecb54e
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 11:56:16 -0400

    Restore game attendance and weekly defaults

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v19";
+const CACHE_NAME = "fbp-shell-v20";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
