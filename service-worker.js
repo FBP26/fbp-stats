5655c013d42576da55d436ab936912322b34c047
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 17:16:34 -0400

    Refresh missing team records immediately

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v24";
+const CACHE_NAME = "fbp-shell-v25";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
