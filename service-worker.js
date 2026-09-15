40bdbfedde4104d530bed427f5633e9533db9159
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 14:33:06 -0400

    Refresh stale active slates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v21";
+const CACHE_NAME = "fbp-shell-v22";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
