bea5d5ed446444f439c10e7835a784ee1e7d7a1d
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 16:44:37 -0400

    Hide rooting matrix until ready

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v22";
+const CACHE_NAME = "fbp-shell-v23";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
