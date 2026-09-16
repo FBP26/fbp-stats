234108743d94478a1747f5f82d6c0b4ca1f1d7ba
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-15 23:17:47 -0400

    Clarify live tiebreak status

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v26";
+const CACHE_NAME = "fbp-shell-v27";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
