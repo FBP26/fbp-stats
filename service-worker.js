301d3d127798bd73076443b12f393db0887576e3
jimoc86vs <jimoc86vs@users.noreply.github.com>
2026-09-23 10:59:21 -0400

    Keep alert player suggestions unique after roster loading

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fbp-shell-v32";
+const CACHE_NAME = "fbp-shell-v33";
 const APP_SHELL = ["./", "./index.html", "./historical-ui.js", "./teams.js", "./map.js", "./map.css", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
